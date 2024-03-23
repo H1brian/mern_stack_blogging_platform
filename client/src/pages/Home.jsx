81a1bb42adf23c5c66e304efb16029af11669cf4
H1brian <133236039+H1brian@users.noreply.github.com>
2024-03-22 23:16:52 -0400

    Add component Header

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { Button } from 'flowbite-react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+    <div className='text-blue-400 text-xl border-b-2'>Home</div>
+    <Button className='text-red-600 text-3xl' color={'failure'} pill gradientDuoTone="tealToLime">Click me</Button>
+    </div>
   )
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
+    // tailwind CSS orgin
+    "./index.html", 
     "./src/**/*.{js,ts,jsx,tsx}",
+    // Flowbite react
+    'node_modules/flowbite-react/lib/esm/**/*.js',
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin'),
+  ],
 }
 
