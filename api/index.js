3b15cf35bb97459ef10b70cbfa9c1f9755c33068
H1brian <133236039+H1brian@users.noreply.github.com>
2024-03-23 23:46:25 -0400

    Connect to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import mongoose, { mongo } from 'mongoose'
 import 'dotenv/config';
 
+mongoose
+    .connect(process.env.DATABASE_CLOUD)
+    .then(() => {
+        console.log('MongoDB database is connected');
+    })
+    .catch((err) => {
+        console.log(err);
+    })
 const app = express();
+
 
 const port = process.env.PORT
 app.listen(port, () => {
