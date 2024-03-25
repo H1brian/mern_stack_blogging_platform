6877d150856c262fa4b81fcc822a71cca769fdb9
H1brian <133236039+H1brian@users.noreply.github.com>
2024-03-25 12:10:25 -0400

    Add hashed password

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,15 +1,28 @@
 import User from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';  // no 'js' added to the end of the name will result in error
 
 export const signup = async (req, res) => {
     // res.json({ message: 'Running well'})
     const { username, email, password } = req.body;
 
+    // Handling the error of required fields
+    if (
+        !username || !email || !password || 
+        username === '' || email === '' || password === ''
+    ) {
+        return res.status(400).json({message: 'All fields are required'})
+    }
+
+    // Hash the password
+    const hashedPassword = bcryptjs.hashSync(password, 10);   // 10 is the number of salts which will be mixed with the password
+
+    // Create a new user using 'User' model
     const newUser = new User({
         username,
         email,
-        password
+        password: hashedPassword
     });
-
+    // Handling the response and error
     try {
         await newUser.save();
         res.json({ message: "Signup successfully!"});
