6cd2ca2a677f20cb55e8560773dcf1da08b87540
H1brian <133236039+H1brian@users.noreply.github.com>
2024-03-24 22:03:25 -0400

    Create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
-import mongoose, { mongo } from 'mongoose'
-import 'dotenv/config';
+import mongoose, { mongo } from 'mongoose';
+import 'dotenv/config'; // Using the data written in .env file
+import userRoutes from './routes/user.route.js';  // extension ".js" is a must for backend
 
 mongoose
     .connect(process.env.DATABASE_CLOUD)
@@ -13,8 +14,10 @@ mongoose
     
 const app = express();
 
+app.use('/api/user', userRoutes);
 
 const port = process.env.PORT
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 });
+
